f8e698d42da693dd211e2ef73f8f2e637a2c7314
Sourav <chakrabortysourav801@gmail.com>
2021-09-14 01:54:17 +0530

    basic user data validation by express-validator & preventing duplicate user

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@
 const mongoose=require('mongoose')
 
 //take the url from compass by manually filling the field
-const mongoURI="mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoURI="mongodb://localhost:27017/EDiary?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
 const connectToMongo=()=>{
     mongoose.connect(mongoURI,()=>{
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,16 +2,40 @@
 
 var express = require('express');
 const User=require('../models/User')
+//using express validator for basic validation
+const { body, validationResult } = require('express-validator');
+
 var router = express.Router();
+
 
 //creating a new user,post request (/api/auth),does not need authentication
 
-router.post('/', function (req, res) {
-    console.log(req.body)
-    const user=new User(req.body)
-    user.save()
-    res.send(req.body);
-  })
+
+
+router.post('/',[
+  //putting all the value that need to varify in an array.use boiler plate code of express-validator home page for this
+  body('email','Enter a valid email').isEmail(),
+  body('name','name must has atleast 2 charecter').isLength({ min: 2 }),
+  body('password','password must has atleast 5 charecters').isLength({ min: 5 })
+], function (req, res) {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      User.create({//boiler plate code from express-validator website
+        name: req.body.name,
+        email:req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user))
+      .catch(err=>{
+        console.log(err)
+        res.json({error:"please enter an unique value"})
+      
+      });
+    }
+      
+  )
 
 
 module.exports = router;
