e48020d13ddc51dab5fceff5de4008de1056affa
Sourav <chakrabortysourav801@gmail.com>
2021-09-14 17:12:16 +0530

    jwt web token done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,11 +4,14 @@ var express = require("express");
 const User = require("../models/User");
 //using express validator for basic validation
 const { body, validationResult } = require("express-validator");
-const bcrypt=require('bcryptjs')
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
 
 var router = express.Router();
 
 //creating a new user,post request (/api/auth),does not need authentication
+
+const JWTSecret = "$$You are not $welcome&&#";//jwt signature
 
 router.post(
   "/createuser",
@@ -26,7 +29,7 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
     try {
-      let user = await User.findOne({ email: req.body.email });//searching user with same email
+      let user = await User.findOne({ email: req.body.email }); //searching user with same email
 
       if (user) {
         return res
@@ -34,16 +37,28 @@ router.post(
           .json({ msg: "User already exists with same email" });
       }
 
-      const salt=await bcrypt.genSaltSync(10);//generating salt
-      const hash =await bcrypt.hashSync(req.body.password, salt);//hashing the password
+      const salt = await bcrypt.genSaltSync(10); //generating salt
+      const hash = await bcrypt.hashSync(req.body.password, salt); //hashing the password
       user = await User.create({
         //boiler plate code from express-validator website
         name: req.body.name,
         email: req.body.email,
-        password: hash,//storing the hashed password
-      })
-      
-      res.json({ msg: "User created" });
+        password: hash, //storing the hashed password
+      });
+
+
+      /* will use jwt web token to identify all authentic user ,for jwt we need id of the user & a seckret sign*/
+
+      //user id ,we are using for jwt 
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, JWTSecret);//jwtSecret is our own signature,it will help us to identify user 
+
+      res.json({ authToken });
     } catch (err) {
       console.error(err);
     }
